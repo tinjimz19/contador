25a377791c4518258415296b0ed1b17ec465d228
Martin Jimenez <jimenito.2014.mj@gmail.com>
2021-09-13 01:43:37 -0400

    Countdown 1.0

--- functions.js
+++ functions.js
@@ -3,7 +3,7 @@ $(function () {
         COUNTDOWN 4
      ========================================= */
 
-    $('#clock-c').countdown('2030/1/1').on('update.countdown', function(event) {
+    $('#clock-c').countdown('2071/9/9').on('update.countdown', function(event) {
       var $this = $(this).html(event.strftime(''
         + '<span class="h1 font-weight-bold">%m</span> mese%!m'
         + '<span class="h1 font-weight-bold">%d</span> Dia%!d'
